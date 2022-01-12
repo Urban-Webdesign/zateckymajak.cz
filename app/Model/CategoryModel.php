d9fd7bcf395c10fd2e3296ea901f8f077e836860
Filip Urban <fifaurb@Filips-MacBook-Air.local>
2022-01-12 02:36:42 +0100

    gitignore

---
+++ app/Model/CategoryModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Model;
+
+use K2D\Core\Models\BaseModel;
+use Nette\Database\Table\ActiveRow;
+use Nette\Database\Table\Selection;
+
+class ArticleModel extends BaseModel
+{
+	protected string $table = 'article';
+
+
+}
